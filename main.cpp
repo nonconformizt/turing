31eee04f3a3384c7bc17a881919379b41d927fc3
Bogdan <bogdankorzh254@mail.ru>
2020-01-21 22:38:41 +0300

    Style resource fix

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 
     a.setFont(QFont ("Lato", 11, QFont::Normal));
 
-    QFile Style("/home/non/dev/qt/Turing/style.qss");
+    QFile Style(":/style/style.qss");
     Style.open(QFile::ReadOnly);
     QString stylesheet = QLatin1String(Style.readAll());
     a.setStyleSheet(stylesheet);
